9d53ae13b01b2f99242ae8e19dd44f100c4e4d52
Guest User <Guest@epicodus-26.local>
2018-05-29 17:50:41 -0700

    added 3 photos with hide.show function

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,20 @@ $(function() {
     $("#walrus-hidden").slideToggle("slow");
   });
 });
+
+$(function(){
+  $(".img1").click(function(){
+    $("#show1").toggle();
+    $("#hidden1").toggle();
+  });
+
+  $(".img2").click(function(){
+    $("#show2").toggle();
+    $("#hidden2").toggle();
+  });
+
+  $(".img3").click(function(){
+    $("#show3").toggle();
+    $("#hidden3").toggle();
+  });
+});
